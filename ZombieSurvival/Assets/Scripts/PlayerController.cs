faf73788f1b485106054b973c7bc32b61942a1f3
Mike Thal <mikeathal@gmail.com>
2016-07-25 15:49:29 -0400

    Player updated with code for moving and for looking at mouse cursor

--- ZombieSurvival/Assets/Scripts/PlayerController.cs
+++ ZombieSurvival/Assets/Scripts/PlayerController.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	// Use this for initialization
+	public float PlayerSpeed = 5f;
+	Vector2 PlayerMoveVector = new Vector2(0f, 0f);
+	Rigidbody2D PlayerRigidBody;
+
 	void Start () {
-	
+		PlayerRigidBody = GetComponent<Rigidbody2D>();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+		PlayerMoveVector = new Vector2( horizontal, vertical );
+		if ( PlayerMoveVector.magnitude > 1f ) {
+			PlayerMoveVector = PlayerMoveVector.normalized; // better way of normalization
+		}
+	}
+
+	// FixedUpdate is when Physics runs
+	void FixedUpdate () {
+		PlayerRigidBody.velocity = PlayerMoveVector * PlayerSpeed;
 	}
 }
---
+++ ZombieSurvival/Assets/Scripts/PlayerLookAtCursor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerLookAtCursor : MonoBehaviour {
+
+	public Transform Player;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 LookDirection = Camera.main.ScreenToWorldPoint( Input.mousePosition);
+		LookDirection.z = 0f;
+
+		Player.transform.up = LookDirection;
+	}
+}
