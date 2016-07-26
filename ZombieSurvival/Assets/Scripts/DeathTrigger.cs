63a7247289fc9f1562bc46a974fdbf48e959a94e
Mike Thal <mikeathal@gmail.com>
2016-07-26 16:04:22 -0400

    Zombies can now kill the player.  To clarify, the player object is NOT being destroyed.  The Sprites are being disabled.

---
+++ ZombieSurvival/Assets/Scripts/DeathTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathTrigger : MonoBehaviour {
+
+	// OnTriggerEnter2D a function that is automatically called when
+	// something with a Rigidbody2D enters this trigger
+	void OnTriggerEnter2D( Collider2D activator ) {
+		// Does the activating thing have a Killable script on it?
+		if (activator.gameObject.tag == "Player") {
+			// TODO: subtract health from the Killable script
+
+			// destroy this object
+			activator.GetComponent<SpriteRenderer>().enabled = false;
+		}
+	}
+}
